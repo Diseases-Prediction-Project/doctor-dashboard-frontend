3f4d218b514e20a5f1480fde1f322fdabceab542
VKwamboka <verahnyagaka101@gmail.com>
2025-05-31 21:11:19 +0200

    updated page style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
 import App from './App';
 import './App.css';
 
